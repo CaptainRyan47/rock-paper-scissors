61982fd112f11b75da15e96fc9aeb185de9ec299
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-04-19 15:22:30 -0700

    Create game function

--- script.js
+++ script.js
@@ -55,24 +55,37 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// Game function:
-//  print Let's play Rock Paper Scissors! Best 3 out of 5.
-//  Loop while player score < 3 and computer score < 3
-//    computer choice function
-//    player choice function
-//    play round function
-//    if win
-//      add 1 to player score
-//      print you won this round!
-//    if lose
-//      add 1 to computer score
-//      print you lost this round.
-//    if tie
-//      add 1 to tie counter
-//      print its a draw.
-//  if player score >= 3
-//    print player score and computer score and tie counter
-//    print You WIN
-//  if computer score >= 3
-//    print player score and computer score and tie counter
-//    print You lose :(
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let tieCounter = 0;
+  let result;
+  console.log('Let\'s play Rock Paper Scissors! Best 3 out of 5.')
+  while (playerScore < 3 && computerScore < 3) {
+    result = playRound(playerChoice(), getComputerChoice());
+    if (result === 'win') {
+      playerScore++;
+      console.log(result);
+    }
+    if (result === 'lose') {
+      computerScore++;
+      console.log(result);
+    }
+    if (result === 'tie') {
+      tieCounter++;
+      console.log(result);
+    }
+  }
+  if (playerScore >= 3) {
+    console.log('You win!');
+    console.log('Your Score: ' + playerScore);
+    console.log('ComputerScore: ' + computerScore);
+  }
+  if (computerScore >= 3) {
+    console.log('You Lose :(');
+    console.log('Your Score: ' + playerScore);
+    console.log('ComputerScore: ' + computerScore);
+  }
+}
+
+game();
